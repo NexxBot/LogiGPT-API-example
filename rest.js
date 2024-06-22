1c6ebcbbe1e39706ea4c0bdfc72366e8742fa293
YangXuepei <1155107976@link.cuhk.edu.hk>
2024-06-22 13:49:12 +0800

    fix comments

--- rest.js
+++ rest.js
@@ -29,8 +29,8 @@ axios.post("http://" + host + ":5001/message/session", {}, {
         console.log(message);
         message = JSON.parse(message);
         message_id = message.data.id;
-        可以以消息状态变为2作为结束的标识，也可以以流结束作为结束标识
-        如果有更多可视化图表则可视化
+        // 可以以消息状态变为2作为结束的标识，也可以以流结束作为结束标识
+        // 如果有更多可视化图表则可视化
         if (message.data.status == 2 && message.data.has_more_to_visualize == true) {
             axios.post("http://" + host + ":5001/message/visualization", {
                 message_id: message.data.id,
